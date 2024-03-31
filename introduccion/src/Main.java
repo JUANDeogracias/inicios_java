f8fc6d909cab216955c56ea6c50d4a4ab82c9528
juan <jdeomoya@gmail.com>
2024-03-31 18:36:27 +0200

    inicializado el repositorio en git

---
+++ introduccion/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        /*Declaracion de variables muy parecido por no decir igual a c
+        * (tipo bool es boolean en java, char al igual que c almacena una sola letra,
+        * por ultimo string almacena palabras)
+        * */
+        byte numOrdenadores = 2;
+        System.out.println("Hay " + numOrdenadores + " ordenadores");
+
+        int edadJuan = 47;
+        float peso= 57.2f;
+        System.out.println("La edad de juan es de " + edadJuan + " años y su peso es de " +
+                peso + " kilos");
+
+        String nombreAcademia = "Mastermind";
+        System.out.println("El nombre de la academia donde estoy aprendiendo es " +
+                nombreAcademia);
+    }
+}
